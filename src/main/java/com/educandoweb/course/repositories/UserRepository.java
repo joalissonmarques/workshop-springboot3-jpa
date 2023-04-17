589cd5c5f84ed25a618880e36283ff9303f6df66
joalissonmarques <joalisonmarques@hotmail.com>
2023-04-17 14:00:00 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -3,7 +3,7 @@ package com.educandoweb.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.User;
-
+//Aqui é opcional colocar o anotation @reposity, pois ele ta herdando do jpa
 public interface UserRepository  extends JpaRepository<User, Long>{
 
 }
