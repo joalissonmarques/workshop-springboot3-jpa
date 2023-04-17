d5ded690091a3f2f27229097a193605f890bb5b2
joalissonmarques <joalisonmarques@hotmail.com>
2023-04-17 15:32:49 -0300

    category

---
+++ src/main/java/com/educandoweb/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.Category;
+//Aqui é opcional colocar o anotation @reposity, pois ele ta herdando do jpa
+public interface CategoryRepository  extends JpaRepository<Category, Long>{
+
+}
